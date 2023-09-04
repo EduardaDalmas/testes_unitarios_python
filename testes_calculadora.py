95e33a85e7eb2e5076537e485251bdea25829bd9
eduardadalmas <eduardadalmas@ienh.com.br>
2023-09-04 20:57:32 -0300

    ajuste teste

--- testes_calculadora.py
+++ testes_calculadora.py
@@ -3,28 +3,29 @@ from calculadora import Calculadora
 
 class TestesCalculadora(unittest.TestCase):
 
-    def __init__(self):
+    def setUp(self):
         self.calculadora = Calculadora()
-        def teste_somar(self):
-            self.calculadora.somar(5)
-            self.assertEqual(self.calculadora.resultado, 5)
 
-        def teste_subtrair(self):
-            self.calculadora.subtrair(3)
-            self.assertEqual(self.calculadora.resultado, -3)
-        
-        def teste_multiplicar(self):
-            self.calculadora.multiplicar(9)
-            self.assertEqual(self.calculadora.resultado, 0)
+    def teste_somar(self):
+        self.calculadora.somar(5)
+        self.assertEqual(self.calculadora.resultado, 5)
 
-        def teste_dividir(self):
-            self.calculadora.dividir(7)
-            self.assertEqual(self.calculadora.resultado, 0)
+    def teste_subtrair(self):
+        self.calculadora.subtrair(3)
+        self.assertEqual(self.calculadora.resultado, -3)
+    
+    def teste_multiplicar(self):
+        self.calculadora.multiplicar(9)
+        self.assertEqual(self.calculadora.resultado, 0)
 
-        def teste_combinacao(self):
-            self.calculadora.somar(10)
-            self.subtrair(5)
-            self.assertEqual(self.calculadora.resultado, 5)
+    def teste_dividir(self):
+        self.calculadora.dividir(7)
+        self.assertEqual(self.calculadora.resultado, 0)
+
+    def teste_combinacao(self):
+        self.calculadora.somar(10)
+        self.subtrair(5)
+        self.assertEqual(self.calculadora.resultado, 5)
 
 if __name__ == '__main__':
     unittest.main()
